05926dad9b925014b799041234c74b751ed0ff2e
Franck Saragosa <126183798+0-UP@users.noreply.github.com>
2023-05-07 18:09:25 +0000

    - Les booléens des valeurs des signes doivent prendre la valeur 0 ou 1. - Légère amélioration de la présentation et ajout de commentaires.
    Prochaine étape : écrire des messages d'erreur.

--- texte.py
+++ texte.py
@@ -38,7 +38,7 @@ def run_texte(ta: table) -> int:
             assert(ta is not None)
     while True:
         cmd = " ".join(filter(None, input("cmd -> ").split())) # enlève tous les espaces en trop
-        if cmd == "set value":
+        if cmd == "set value": # rentrer une valeur dans une case
             try:
                 print("Entrez les coordonnées de la case à modifier")
                 x = demander_un_nombre("x")
@@ -47,28 +47,28 @@ def run_texte(ta: table) -> int:
                 ta.set_value_at(x, y, v)
             except ValueError:
                 continue
-        elif cmd == "set vsign":
+        elif cmd == "set vsign": # mettre un signe |vertical|
             try:
                 print("Quelle est la case se situant EN HAUT du signe à ajouter ?")
                 x = demander_un_nombre("x")
                 y = demander_un_nombre("y")
-                v = int(input("orientation (True pour ⋁, False pour ⋀) : ")) # affichage des caractères spéciaux à vérifier
+                v = int(input("orientation (0 pour ⋀, 1 pour ⋁) : "))
                 ta.set_v_sign_at(x, y, v)
             except ValueError:
                 continue
-        elif cmd == "set hsign":
+        elif cmd == "set hsign": # mettre un signe -horizontal-
             try:
                 print("Quelle est la case se situant À GAUCHE du signe à ajouter ?")
                 x = demander_un_nombre("x")
                 y = demander_un_nombre("y")
-                v = int(input("orientation (True pour >, False pour <) : "))
+                v = int(input("orientation (0 pour <, 1 pour >) : "))
                 ta.set_h_sign_at(x, y, v)
             except ValueError:
                 continue
         elif cmd == "display table": # afficher la table
-            # il suffirait de convertir la table en chaîne de caractères
             try:
-                print(string(ta))
+                print("\n")
+                print(ta)
             except:
                 print("L'opération n'a pas fonctionnée")
         elif cmd == "quit": # mettre fin aux modifications
@@ -85,5 +85,8 @@ def run_texte(ta: table) -> int:
 #    ta.set_h_sign_at(2, 3, False)
 #    ta.set_v_sign_at(3, 2, True) => y_lim=c-2
 #    print(ta)
+    print("Modifications terminées.")
+    print("Aperçu du fichier Dimacs :")
+    print("\n")
     print(ta.gen_dimacs())
     return 0
